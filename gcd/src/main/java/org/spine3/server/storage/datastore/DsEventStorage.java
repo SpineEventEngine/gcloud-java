899379de056e6354f33cecaf422e966553695347
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2016-10-21 17:21:33 +0300

    Add event filtering.

--- gcd/src/main/java/org/spine3/server/storage/datastore/DsEventStorage.java
+++ gcd/src/main/java/org/spine3/server/storage/datastore/DsEventStorage.java
@@ -21,25 +21,32 @@
 package org.spine3.server.storage.datastore;
 
 import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.GqlQuery;
 import com.google.cloud.datastore.Key;
 import com.google.cloud.datastore.Query;
-import com.google.cloud.datastore.StructuredQuery.Filter;
-import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
 import com.google.common.base.Function;
-import com.google.common.collect.Iterators;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.protobuf.Any;
+import com.google.protobuf.Message;
 import org.spine3.base.Event;
 import org.spine3.base.EventId;
+import org.spine3.protobuf.AnyPacker;
+import org.spine3.protobuf.Messages;
+import org.spine3.protobuf.Timestamps;
 import org.spine3.protobuf.TypeUrl;
+import org.spine3.server.event.EventFilter;
 import org.spine3.server.event.EventStreamQuery;
+import org.spine3.server.event.EventStreamQueryOrBuilder;
 import org.spine3.server.storage.EventStorage;
 import org.spine3.server.storage.EventStorageRecord;
 import org.spine3.server.storage.datastore.newapi.DatastoreWrapper;
 
 import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 
-import static com.google.cloud.datastore.StructuredQuery.CompositeFilter;
-import static com.google.cloud.datastore.StructuredQuery.OrderBy;
 import static org.spine3.base.Identifiers.idToString;
 import static org.spine3.server.storage.datastore.DatastoreProperties.TIMESTAMP_NANOS_PROPERTY_NAME;
 import static org.spine3.server.storage.datastore.newapi.Entities.entityToMessage;
@@ -57,6 +64,33 @@ class DsEventStorage extends EventStorage {
     private final DatastoreWrapper datastore;
     private static final String KIND = EventStorageRecord.class.getName();
     private static final TypeUrl TYPE_URL = TypeUrl.of(EventStorageRecord.getDescriptor());
+    private static final int SQL_INITIAL_LENGTH = 128;
+
+    private static final Function<Entity, Event> ENTITY_TO_EVENT = new Function<Entity, Event>() {
+        @Nullable
+        @Override
+        public Event apply(@Nullable Entity entityResult) {
+            if (entityResult == null) {
+                return Event.getDefaultInstance();
+            }
+            final EventStorageRecord message = entityToMessage(entityResult, TYPE_URL);
+            final Event result = toEvent(message);
+            return result;
+        }
+    };
+
+    private static final Function<Any, String> ID_TRANSFORMER = new Function<Any, String>() {
+        @Nullable
+        @Override
+        public String apply(@Nullable Any input) {
+            if (input == null) {
+                return null;
+            }
+
+            final Message messageId = AnyPacker.unpack(input);
+            return Messages.toText(messageId);
+        }
+    };
 
     /* package */
     static DsEventStorage newInstance(DatastoreWrapper datastore, boolean multitenant) {
@@ -68,24 +102,55 @@ class DsEventStorage extends EventStorage {
         this.datastore = datastore;
     }
 
+    private static Predicate<Event> eventPredicate(EventStreamQuery query) {
+        final Collection<String> eventTypes = new HashSet<>(query.getFilterCount());
+        final Collection<String> aggregateIds = new HashSet<>(query.getFilterCount());
+
+        for (EventFilter filter : query.getFilterList()) {
+            final Collection<String> stringIds = Collections2.transform(filter.getAggregateIdList(), ID_TRANSFORMER);
+            aggregateIds.addAll(stringIds);
+            eventTypes.add(filter.getEventType());
+        }
+
+        // TODO:21-10-16:dmytro.dashenkov: Field filters (for both context and message).
+
+        return new EventPredicate(eventTypes, aggregateIds);
+    }
+
     @Override
     public Iterator<Event> iterator(EventStreamQuery eventStreamQuery) {
-        final Filter afterFilter = PropertyFilter.gt(
-                TIMESTAMP_NANOS_PROPERTY_NAME,
-                eventStreamQuery.getAfter().getNanos());
-        final Filter beforeFilter = PropertyFilter.lt(
-                TIMESTAMP_NANOS_PROPERTY_NAME,
-                eventStreamQuery.getBefore().getNanos());
-        final Filter boundsFilter = CompositeFilter.and(afterFilter, beforeFilter);
-        final Query query = Query.entityQueryBuilder()
-                .kind(KIND)
-                .orderBy(OrderBy.asc(TIMESTAMP_NANOS_PROPERTY_NAME))
-                .filter(boundsFilter)
-                .build();
-        final Iterator<com.google.cloud.datastore.Entity> iterator = datastore.read(query).iterator();
-        final Iterator<Event> transformedIterator = Iterators.transform(iterator, ENTITY_TO_EVENT);
+        final Query query = toTimestampQuery(eventStreamQuery);
 
-        return transformedIterator;
+        final Collection<Entity> entities = datastore.read(query);
+        // TODO:21-10-16:dmytro.dashenkov: Not optimal. Should first filter then transform.
+        Collection<Event> events = Collections2.transform(entities, ENTITY_TO_EVENT);
+        events = Collections2.filter(events, eventPredicate(eventStreamQuery));
+
+        final Iterator<Event> iterator = events.iterator();
+        return iterator;
+
+    }
+
+    @SuppressWarnings({"MethodWithMoreThanThreeNegations", "ValueOfIncrementOrDecrementUsed", "DuplicateStringLiteralInspection"})
+    private static Query toTimestampQuery(EventStreamQueryOrBuilder query) {
+        final long lower = Timestamps.convertToNanos(query.getAfter());
+        final long upper = query.hasBefore()
+                ? Timestamps.convertToNanos(query.getBefore())
+                : Long.MAX_VALUE;
+
+        final StringBuilder sql = new StringBuilder(SQL_INITIAL_LENGTH);
+        sql.append("SELECT * FROM ")
+                .append(TYPE_URL.getSimpleName())
+                .append(" WHERE ")
+                .append(TIMESTAMP_NANOS_PROPERTY_NAME)
+                .append(" > @1")
+                .append(" AND ")
+                .append(TIMESTAMP_NANOS_PROPERTY_NAME)
+                .append(" < @2");
+        final GqlQuery.Builder gqlQuery = Query.gqlQueryBuilder(sql.toString())
+                .addBinding(lower)
+                .addBinding(upper);
+        return gqlQuery.build();
     }
 
     @Override
@@ -120,16 +185,43 @@ class DsEventStorage extends EventStorage {
         return result;
     }
 
-    private static final Function<Entity, Event> ENTITY_TO_EVENT = new Function<Entity, Event>() {
-        @Nullable
-        @Override
-        public Event apply(@Nullable Entity entityResult) {
-            if (entityResult == null) {
-                return Event.getDefaultInstance();
-            }
-            final EventStorageRecord message = entityToMessage(entityResult, TYPE_URL);
-            final Event result = toEvent(message);
-            return result;
+    private static class EventPredicate implements Predicate<Event> {
+
+        private final Collection<String> eventTypes;
+        private final Collection<String> aggregateIds;
+
+        private EventPredicate(Collection<String> eventTypes, Collection<String> aggregateIds) {
+            this.eventTypes = eventTypes;
+            this.aggregateIds = aggregateIds;
         }
-    };
+
+        @Override
+        public boolean apply(@Nullable Event event) {
+            if (event == null) {
+                return false;
+            }
+
+            final Any eventAny = event.getMessage();
+            final Message eventMessage = AnyPacker.unpack(eventAny);
+            final String eventType = eventMessage.getDescriptorForType().getFullName();
+
+            final boolean typeMatches = eventTypes.contains(eventType);
+            if (!typeMatches) {
+                return false;
+            }
+
+            if (!aggregateIds.isEmpty()) {
+                final Any aggregateIdAny = event.getContext().getProducerId();
+                final Message aggregateId = AnyPacker.unpack(aggregateIdAny);
+                final String aggregateIdString = Messages.toText(aggregateId);
+
+                final boolean idMatches = aggregateIds.contains(aggregateIdString);
+                if (!idMatches) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
 }
