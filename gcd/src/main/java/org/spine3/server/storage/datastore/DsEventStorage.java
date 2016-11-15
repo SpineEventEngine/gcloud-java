aeed433b106682fec00ce02011ca5a650ff4ccd9
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2016-11-15 11:27:52 +0200

    Merge remote.

--- gcd/src/main/java/org/spine3/server/storage/datastore/DsEventStorage.java
+++ gcd/src/main/java/org/spine3/server/storage/datastore/DsEventStorage.java
@@ -20,9 +20,9 @@
 
 package org.spine3.server.storage.datastore;
 
-import com.google.api.client.repackaged.com.google.common.base.Strings;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
 import com.google.cloud.datastore.Query;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
