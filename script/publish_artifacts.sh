431e4986ebd87328f4442ddc0ca033c35f070112
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2016-11-23 17:35:23 +0200

    Update publishing credentials enabling automatic artifact publishing for `gae-java`.

--- script/publish_artifacts.sh
+++ script/publish_artifacts.sh
@@ -10,7 +10,7 @@ echo " -- PUBLISHING: current branch is $TRAVIS_BRANCH."
 
 if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo " ------ Publishing the artifacts to the repository..."
-    openssl aes-256-cbc -K $encrypted_0f12c1faf1fc_key -iv $encrypted_0f12c1faf1fc_iv -in credentials.properties.enc -out credentials.properties -d
+    openssl aes-256-cbc -K $encrypted_0561e108020c_key -iv $encrypted_0561e108020c_iv -in credentials.properties.enc -out credentials.properties -d
     ./gradlew publish -x test
     echo " ------ Artifacts published."
 else
