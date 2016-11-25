9d4c6ca5c5e51ca644ba1adb4743a46a8bfbb842
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2016-11-25 11:29:46 +0200

    Set correct default project name during the Windows-based Datastore emulator start.

--- script/start-datastore.bat
+++ script/start-datastore.bat
@@ -1,1 +1,1 @@
-gcloud beta emulators datastore start --project=spine --host-port=localhost:8080 --consistency 1.0 --no-store-on-disk
+gcloud beta emulators datastore start --project=spine-dev --host-port=localhost:8080 --consistency 1.0 --no-store-on-disk
