71b599394701c0021677c6fa26296ba91a15ab35
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-01-06 17:57:47 +0200

    Change consistency await time offset.

--- gcd/src/test/java/org/spine3/server/storage/datastore/TestDatastoreWrapper.java
+++ gcd/src/test/java/org/spine3/server/storage/datastore/TestDatastoreWrapper.java
@@ -45,8 +45,8 @@ class TestDatastoreWrapper extends DatastoreWrapper {
 
     // Default time to wait before each read operation to ensure the data is consistent.
     // NOTE: enabled only if {@link #shouldWaitForConsistency} is {@code true}.
-    private static final int CONSISTENCY_AWAIT_TIME_MS = 100;
-    private static final int CONSISTENCY_AWAIT_ITERATIONS = 8;
+    private static final int CONSISTENCY_AWAIT_TIME_MS = 75;
+    private static final int CONSISTENCY_AWAIT_ITERATIONS = 20;
 
     /**
      * Due to eventual consistency, {@link #dropTable(String) is performed iteratively until the table has no records}.
