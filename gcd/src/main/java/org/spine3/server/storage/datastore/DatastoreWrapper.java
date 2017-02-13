e79ae38ae47c1006906193c17b569b1316112412
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-02-13 16:53:15 +0200

    Simplify if( instanceof ) chain.

--- gcd/src/main/java/org/spine3/server/storage/datastore/DatastoreWrapper.java
+++ gcd/src/main/java/org/spine3/server/storage/datastore/DatastoreWrapper.java
@@ -35,6 +35,7 @@ import com.google.cloud.datastore.KeyQuery;
 import com.google.cloud.datastore.ProjectionEntityQuery;
 import com.google.cloud.datastore.Query;
 import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery;
 import com.google.cloud.datastore.Transaction;
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
@@ -188,7 +189,7 @@ public class DatastoreWrapper {
      * @return results fo the query packed in a {@link List}
      * @see DatastoreReader#run(Query)
      */
-    @SuppressWarnings({"unchecked", "IfStatementWithTooManyBranches", "ChainOfInstanceofChecks"})
+    @SuppressWarnings("unchecked")
     public List<Entity> read(Query query) {
         QueryResults queryResults = actor.run(query);
         final List<Entity> resultsAsList = newLinkedList();
@@ -208,19 +209,9 @@ public class DatastoreWrapper {
              * the {@code Builder}s, allowing to inject a new {@code startCursor} value.
              **/
 
-            if (query instanceof EntityQuery) {
-                final EntityQuery entityQuery = (EntityQuery) query;
-                queryForMoreResults = entityQuery.toBuilder()
-                        .setStartCursor(cursorAfter)
-                        .build();
-            } else if (query instanceof KeyQuery) {
-                final KeyQuery keyQuery = (KeyQuery) query;
-                queryForMoreResults = keyQuery.toBuilder()
-                        .setStartCursor(cursorAfter)
-                        .build();
-            } else if (query instanceof ProjectionEntityQuery) {
-                final ProjectionEntityQuery peQuery = (ProjectionEntityQuery) query;
-                queryForMoreResults = peQuery.toBuilder()
+            if (query instanceof StructuredQuery) {
+                final StructuredQuery structuredQuery = (StructuredQuery) query;
+                queryForMoreResults = structuredQuery.toBuilder()
                         .setStartCursor(cursorAfter)
                         .build();
             } else {
