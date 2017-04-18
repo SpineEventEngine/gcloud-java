f850ea44b0642371f06650aba5197943d006459c
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-04-18 16:41:03 +0300

    Add yet more Javadoc.

--- gcd/src/main/java/org/spine3/server/storage/datastore/tenant/NamespaceToTenantIdConverter.java
+++ gcd/src/main/java/org/spine3/server/storage/datastore/tenant/NamespaceToTenantIdConverter.java
@@ -27,25 +27,39 @@ import org.spine3.users.TenantId;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- * // TODO:2017-04-18:dmytro.dashenkov: Javadoc.
+ * A {@link Converter} for {@link TenantId} and the string Datastore namespace.
+ *
+ * <p>Define this converter in case if there is a need to override
+ * the {@linkplain Namespace default convertion behavior}.
+ *
+ * <p>The implementation of the convertion must be commutative, meaning that the restored
+ * {@link TenantId} values should be {@linkplain TenantId#equals(Object) equal} to the initial.
  *
  * @author Dmytro Dashenkov
+ * @see Namespace
  */
 @SPI
 public abstract class NamespaceToTenantIdConverter extends Converter<String, TenantId> {
 
     @Override
-    protected TenantId doForward(String s) {
+    protected final TenantId doForward(String s) {
         checkNotNull(s);
         return toTenantId(s);
     }
 
     @Override
-    protected String doBackward(TenantId tenantId) {
+    protected final String doBackward(TenantId tenantId) {
         checkNotNull(tenantId);
         return toString(tenantId);
     }
 
+    /**
+     * Converts the given {@link TenantId} into a string representing a Datastore namespace.
+     */
     protected abstract String toString(TenantId tenantId);
+
+    /**
+     * Converts the given string representing a Datastore namespace into a {@link TenantId}.
+     */
     protected abstract TenantId toTenantId(String namespace);
 }
