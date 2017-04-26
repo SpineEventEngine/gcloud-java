c2d476c0ccba100f8790303f736b29189b626f19
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-04-26 16:15:39 +0300

    Add a clarifying comment.

--- gcd/src/main/java/org/spine3/server/storage/datastore/tenant/DatastoreTenants.java
+++ gcd/src/main/java/org/spine3/server/storage/datastore/tenant/DatastoreTenants.java
@@ -77,6 +77,7 @@ public final class DatastoreTenants {
      */
     public static TenantIndex index(Datastore datastore) {
         checkNotNull(datastore);
+        // We assume a single-tenant execution environment
         final TenantIndex index = new NamespaceIndex(datastore, true);
         return index;
     }
