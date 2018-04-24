4c31e24ad5e96e2827067a5e4a927a50beb81f48
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2018-04-24 17:16:30 +0300

    Add some tests for the `CommandServlet`.

--- web/src/test/java/io/spine/web/CommandServletTest.java
+++ web/src/test/java/io/spine/web/CommandServletTest.java
@@ -6,18 +6,29 @@
 
 package io.spine.web;
 
-import io.spine.server.BoundedContext;
-import io.spine.server.CommandService;
-import io.spine.testdata.TestBoundedContextFactory;
-import org.junit.jupiter.api.BeforeEach;
+import io.spine.base.Time;
+import io.spine.client.CommandFactory;
+import io.spine.client.TestActorRequestFactory;
+import io.spine.core.Ack;
+import io.spine.core.Command;
+import io.spine.json.Json;
+import io.spine.protobuf.AnyPacker;
+import io.spine.web.given.CommandServletTestEnv.TestCommandServlet;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.StringWriter;
 
+import static io.spine.core.Status.StatusCase.OK;
+import static io.spine.web.given.Servlets.request;
+import static io.spine.web.given.Servlets.response;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Dmytro Dashenkov
@@ -25,21 +36,36 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @DisplayName("CommandServlet should")
 class CommandServletTest {
 
-    private CommandService commandService;
-
-    @BeforeEach
-    void setUp() {
-        final BoundedContext bc = TestBoundedContextFactory.MultiTenant.newBoundedContext();
-        commandService = CommandService.newBuilder()
-                                       .add(bc)
-                                       .build();
-    }
+    private static final CommandFactory commandFactory =
+            TestActorRequestFactory.newInstance(CommandServletTest.class)
+                                   .command();
 
     @Test
     @DisplayName("fail to serialize")
     void testSerialize() throws IOException {
-        final CommandServlet servlet = new CommandServlet(commandService) {};
+        final CommandServlet servlet = new TestCommandServlet();
         final ObjectOutputStream stream = new ObjectOutputStream(new ByteArrayOutputStream());
         assertThrows(UnsupportedOperationException.class, () -> stream.writeObject(servlet));
+    }
+
+    @Test
+    @DisplayName("handle `/command` POST requests")
+    void testHandle() throws IOException {
+        final CommandServlet servlet = new TestCommandServlet();
+        final StringWriter response = new StringWriter();
+        final Command command = commandFactory.create(Time.getCurrentTime());
+        servlet.doPost(request(command), response(response));
+        final Ack ack = Json.fromJson(response.toString(), Ack.class);
+        assertEquals(OK, ack.getStatus().getStatusCase());
+        assertEquals(command.getId(), AnyPacker.unpack(ack.getMessageId()));
+    }
+
+    @Test
+    @DisplayName("respond 400 to an invalid command")
+    void testInvalidCommand() throws IOException {
+        final CommandServlet servlet = new TestCommandServlet();
+        final HttpServletResponse response = response(new StringWriter());
+        servlet.doPost(request(Time.getCurrentTime()), response);
+        verify(response).sendError(400);
     }
 }
---
+++ web/src/test/java/io/spine/web/given/CommandServletTestEnv.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.web.given;
+
+import io.grpc.stub.StreamObserver;
+import io.spine.core.Ack;
+import io.spine.core.Command;
+import io.spine.protobuf.AnyPacker;
+import io.spine.server.CommandService;
+import io.spine.web.CommandServlet;
+
+import static io.spine.core.Responses.statusOk;
+import static io.spine.test.Tests.nullRef;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author Dmytro Dashenkov
+ */
+public final class CommandServletTestEnv {
+
+    /**
+     * Prevents the utility class instantiation.
+     */
+    private CommandServletTestEnv() {
+    }
+
+    @SuppressWarnings("unchecked") // Mocking generics.
+    public static CommandService positiveCommandService() {
+        final CommandService commandService = mock(CommandService.class);
+        doAnswer(invocation -> {
+            final StreamObserver<Ack> observer = invocation.getArgument(1);
+            final Command command = invocation.getArgument(0);
+            observer.onNext(Ack.newBuilder()
+                               .setMessageId(AnyPacker.pack(command.getId()))
+                               .setStatus(statusOk())
+                               .build());
+            observer.onCompleted();
+            return nullRef();
+        }).when(commandService).post(any(Command.class), any(StreamObserver.class));
+        return commandService;
+    }
+
+    @SuppressWarnings("serial")
+    public static final class TestCommandServlet extends CommandServlet {
+
+        public TestCommandServlet() {
+            super(positiveCommandService());
+        }
+    }
+}
