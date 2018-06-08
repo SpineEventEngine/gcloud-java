814842e654f934d5373a457381905e08f90ea366
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-06-08 19:02:18 +0300

    Add link to `core-java` issue to todo

--- datastore/src/test/java/io/spine/server/storage/datastore/DsFiltersShould.java
+++ datastore/src/test/java/io/spine/server/storage/datastore/DsFiltersShould.java
@@ -169,6 +169,7 @@ public class DsFiltersShould {
     }
 
     //TODO:2018-06-08:dmytro.kuzmin: re-write without mocks when null column filters are available.
+    // See https://github.com/SpineEventEngine/core-java/issues/720.
     @Test
     public void generate_filters_for_null_column_value() {
         final EntityColumn column = mock(EntityColumn.class);
