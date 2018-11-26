640640999f6d09d8a3abe3f31218e50c5caa3331
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-11-26 20:47:50 +0200

    Remove unintended changes in spacing

--- datastore/src/test/java/io/spine/server/storage/datastore/given/TestDatastores.java
+++ datastore/src/test/java/io/spine/server/storage/datastore/given/TestDatastores.java
@@ -17,6 +17,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package io.spine.server.storage.datastore.given;
 
 import com.google.auth.oauth2.ServiceAccountCredentials;
@@ -31,68 +32,85 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import static com.google.auth.oauth2.ServiceAccountCredentials.fromStream;
+
 /**
  * Provides test {@link Datastore} instances.
  */
 public class TestDatastores {
+
     private static final ProjectId TEST_PROJECT_ID = ProjectId.of("spine-dev");
+
     /** Prevent this test utility class from being instantiated. */
     private TestDatastores() {
     }
+
     /**
      * Obtains Datastore instance used for testing on a developer's workstation.
      */
     public static Datastore local() {
         return Local.INSTANCE.getService();
     }
+
     /**
      * Obtains Datastore instance used for testing in a CI environment.
      */
     public static Datastore remote() {
         return Ci.INSTANCE.getService();
     }
+
     /**
      * Obtains ProjectId of the test environment.
      */
     public static ProjectId projectId() {
         return TEST_PROJECT_ID;
     }
+
     /**
      * Abstract base for options factories.
      */
     private abstract static class Options {
+
         private final DatastoreOptions.Builder builder;
+
         private Options() {
             builder = DatastoreOptions.newBuilder()
                                       .setProjectId(projectId().getValue());
         }
+
         final DatastoreOptions.Builder builder() {
             return builder;
         }
+
         final DatastoreOptions create() {
             return builder.build();
         }
     }
+
     /**
      * Local Datastore options used on developers' machines.
      */
     private static final class Local extends Options {
+
         private static final String DEFAULT_HOST = "localhost:8080";
         private static final DatastoreOptions INSTANCE = new Local().create();
+
         private Local() {
             super();
             builder().setHost(DEFAULT_HOST);
             builder().setCredentials(NoCredentials.getInstance());
         }
     }
+
     /**
      * Options used to run tests in CI environment, which uses connection to
      * a real Datastore instance in the testing Google Cloud environment.
      */
     @SuppressWarnings("NewClassNamingConvention")
     private static final class Ci extends Options implements Logging {
+
         private static final String CREDENTIALS_FILE_PATH = "/spine-dev-62685282c0b9.json";
         private static final DatastoreOptions INSTANCE = new Ci().create();
+
         private Ci() {
             try {
                 InputStream is = TestDatastores.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
