42b6473b9018c5420c76a4657b75f2349e459553
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-11-26 15:12:06 +0200

    Fix how the installed SDK is exported to PATH

--- scripts/install-gcloud.sh
+++ scripts/install-gcloud.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Installs the Google Cloud SDK and adds it to PATH, overriding any previous versions if present.
+# Installs the Google Cloud SDK.
 #
 # All output is ignored as the installation process is overly verbose.
 
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 curl https://sdk.cloud.google.com | bash > /dev/null
-source $HOME/google-cloud-sdk/path.bash.inc
