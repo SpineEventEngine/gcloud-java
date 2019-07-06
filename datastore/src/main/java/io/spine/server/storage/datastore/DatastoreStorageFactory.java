7f8191c5293039036af08556379f62e5f6744c4a
Alex Tymchenko <alex.tymchenko@teamdev.com>
2019-07-06 17:11:45 +0300

    Finish the sentence with a period.

--- datastore/src/main/java/io/spine/server/storage/datastore/DatastoreStorageFactory.java
+++ datastore/src/main/java/io/spine/server/storage/datastore/DatastoreStorageFactory.java
@@ -68,7 +68,7 @@ public class DatastoreStorageFactory implements StorageFactory {
 
     private static final String DEFAULT_NAMESPACE_ERROR_MESSAGE =
             "Datastore namespace should not be configured explicitly" +
-                    "for a multitenant storage";
+                    "for a multitenant storage.";
 
     private final Datastore datastore;
 
