5fbc495045663d33d9e3aff832b3a2c7a7469cce
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2019-10-01 17:36:51 +0300

    Remove `CountingDatastoreWrapper` from `testutil-gcloud`
    It's too specific to be in a test lib. Users are better off
    implementing their own versions of the `TestDatastoreWrapper`.

--- datastore/src/test/java/io/spine/server/storage/datastore/given/CountingDatastoreWrapper.java
+++ datastore/src/test/java/io/spine/server/storage/datastore/given/CountingDatastoreWrapper.java
@@ -18,13 +18,14 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.testing.server.storage.datastore;
+package io.spine.server.storage.datastore.given;
 
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
 import com.google.cloud.datastore.StructuredQuery;
 import io.spine.server.storage.datastore.DsQueryIterator;
+import io.spine.testing.server.storage.datastore.TestDatastoreWrapper;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.Collection;
