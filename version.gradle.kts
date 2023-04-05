39bb62a3ec4e34001353d7a0cbf17f9a5ca84dd4
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-04-05 14:32:38 +0100

    Use the latest Spine dependencies.

--- version.gradle.kts
+++ version.gradle.kts
@@ -35,6 +35,6 @@ val cloudDatastoreVersion: String by extra("2.8.0")
 val cloudPubsubV1Version: String by extra("1.101.1")
 val cloudTraceVersion: String by extra("2.2.0")
 
-val spineBaseVersion: String by extra("1.8.2")
-val spineCoreVersion: String by extra("1.8.2")
+val spineBaseVersion: String by extra("1.9.0-SNAPSHOT.5")
+val spineCoreVersion: String by extra("1.9.0-SNAPSHOT.10")
 val versionToPublish: String by extra("1.9.0-SNAPSHOT.1")
