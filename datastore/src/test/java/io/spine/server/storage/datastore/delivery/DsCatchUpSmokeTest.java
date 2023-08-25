62f69d3b2fbd27cd89968fdefc522fe6427e9bb4
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-08-25 18:16:53 +0100

    Reduce the amount of console output.

--- datastore/src/test/java/io/spine/server/storage/datastore/delivery/DsCatchUpSmokeTest.java
+++ datastore/src/test/java/io/spine/server/storage/datastore/delivery/DsCatchUpSmokeTest.java
@@ -30,6 +30,7 @@ import io.spine.environment.Tests;
 import io.spine.server.ServerEnvironment;
 import io.spine.server.delivery.CatchUpTest;
 import io.spine.testing.SlowTest;
+import io.spine.testing.logging.mute.MuteLogging;
 import io.spine.testing.server.storage.datastore.TestDatastoreStorageFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -44,6 +45,7 @@ import org.junit.jupiter.api.Test;
  */
 @SlowTest
 @DisplayName("Datastore-backed `CatchUp` should ")
+@MuteLogging
 class DsCatchUpSmokeTest extends CatchUpTest {
 
     private TestDatastoreStorageFactory factory;
