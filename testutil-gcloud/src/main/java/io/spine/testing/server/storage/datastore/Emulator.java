c5a1a8825ef4990bec2b8f196848b7c3b6b243af
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-08-25 18:18:03 +0100

    Reorganize `Emulator` codebase to extract independent parts into different top-level types (in progress).

--- testutil-gcloud/src/main/java/io/spine/testing/server/storage/datastore/Emulator.java
+++ testutil-gcloud/src/main/java/io/spine/testing/server/storage/datastore/Emulator.java
@@ -1,25 +1,32 @@
 package io.spine.testing.server.storage.datastore;
 
 
-import com.google.cloud.NoCredentials;
-import com.google.cloud.ServiceOptions;
 import com.google.cloud.datastore.DatastoreOptions;
 import io.spine.server.storage.datastore.ProjectId;
-import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
-import org.testcontainers.containers.DatastoreEmulatorContainer;
 import org.testcontainers.utility.DockerImageName;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Manages a Docker-powered Datastore Emulator.
+ *
+ * @implNote This class acts as a JVM-level singleton, operating in {@code static} context,
+ * to make the state shared across all launched JUnit tests. For each {@code port},
+ * a new container is launched.
+ * Once the Java process, from which the container was started, dies,
+ * corresponding Docker container is automatically stopped.
  */
-@SuppressWarnings("resource")
-public final class Emulator {
+final class Emulator {
 
     private static final DockerImageName IMAGE =
             DockerImageName.parse("gcr.io/google.com/cloudsdktool/google-cloud-cli:441.0.0-emulators");
 
-    private static @MonotonicNonNull DatastoreEmulatorContainer emulator = null;
-    private static @MonotonicNonNull DatastoreOptions options = null;
+
+    /**
+     * Currently running emulators, per port on which each of them is running.
+     */
+    private static final Map<Integer, EmulatorContainer> containers = new HashMap<>();
 
     /**
      * Prevents the direct instantiation.
@@ -28,77 +35,26 @@ public final class Emulator {
     }
 
     /**
-     * Returns the connection options to the currently running Datastore Emulator.
+     * Returns the connection options to the running Datastore Emulator.
      *
-     * <p>If no emulator is currently running, a new instance is launched via Docker.
+     * <p>In case there is already an emulator running on the passed port,
+     * returns the connection options to that emulator.
+     *
+     * <p>Otherwise, a new instance is launched via Docker.
+     *
+     * @param port      port on a local machine, at which the emulator is going to be accessed
+     * @param projectId Datastore project ID, which may have an arbitrary value,
+     *                  and not correspond to any real GCP project
      */
     @SuppressWarnings("UseOfSystemOutOrSystemErr")
-    public static synchronized DatastoreOptions options(ProjectId projectId, int port) {
-        if (options == null) {
-            if (emulator == null) {
-                System.out.println("---- Starting a new Datastore Emulator via Docker...");
-                emulator = new Emulator.Container(IMAGE, projectId, port);
-                emulator.start();
-            }
-
-            options = DatastoreOptions.newBuilder()
-                    .setHost(emulator.getEmulatorEndpoint())
-                    .setCredentials(NoCredentials.getInstance())
-                    .setRetrySettings(ServiceOptions.getNoRetrySettings())
-                    .setProjectId(emulator.getProjectId())
-                    .build();
+    static synchronized DatastoreOptions at(int port, ProjectId projectId) {
+        var emulator = containers.get(port);
+        if (emulator == null) {
+            System.out.println("---- Starting a new Datastore Emulator via Docker...");
+            emulator = new EmulatorContainer(IMAGE, projectId, port);
+            emulator.startAndServe();
+            containers.put(port, emulator);
         }
-        return options;
-    }
-
-    /**
-     * A container running the Emulator image.
-     *
-     * <p>The container is started in its "full consistency" mode.
-     */
-    private static class Container extends DatastoreEmulatorContainer {
-
-        /**
-         * A template of BASH command to execute upon Docker container start.
-         */
-        private static final String TEMPLATE =
-                "gcloud beta emulators datastore start " +
-                        "--project %s " +
-                        "--host-port 0.0.0.0:%d " +
-                        "--consistency 1.0";
-
-        private final ProjectId projectId;
-        private final int port;
-
-        /**
-         * Creates a new Emulator container.
-         *
-         * <p>Remember, the instance must be {@linkplain #start() started} after creation.
-         *
-         * @param dockerImageName reference to the Docker image
-         * @param projectId       the GCP project ID
-         * @param port            port to start the emulator on
-         */
-        private Container(DockerImageName dockerImageName, ProjectId projectId, int port) {
-            super(dockerImageName);
-            this.projectId = projectId;
-            this.port = port;
-        }
-
-        @Override
-        protected void configure() {
-            var command = String.format(TEMPLATE, projectId.value(), port);
-            withCommand("/bin/sh", "-c", command);
-        }
-
-        @Override
-        public String getEmulatorEndpoint() {
-            return getHost() + ':' + getMappedPort(port);
-        }
-
-        @Override
-        public String getProjectId() {
-            return projectId.value();
-        }
+        return emulator.options();
     }
 }
