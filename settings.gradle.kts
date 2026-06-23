c0215a19911f46d3ddd96f9f2477847571cb36e3
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-06-23 16:44:01 +0100

    Fix project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-rootProject.name = "spine-gcloud-java"
+rootProject.name = "gcloud-jvm"
 
 include(
     "datastore",
